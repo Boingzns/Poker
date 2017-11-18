65c5432fbd998d84cf6a87ed21183541938cf10d
Boingzns <33784478+Boingzns@users.noreply.github.com>
2017-11-18 15:30:00 +0000

    upate to betting

--- poker money and bettting.py
+++ poker money and bettting.py
@@ -20,6 +20,14 @@ againPlayers = []
 done = []
 betThisRound = False
 ducatsToBet = 0
+lowestPersonsMoney = 0
+
+
+def lowestPossible() :
+    for player in playersLeft :
+        if player.money >= lowestPersonsMoney :
+            player.money >= lowestpersonsMoney :
+                
 
 for i in range(len(playersLeft)-1, -1, -1) :
     player = playersLeft[i]
@@ -31,45 +39,51 @@ for i in range(len(playersLeft)-1, -1, -1) :
 
     if playerChoice == 'fold' :
         playersLeft.remove(player)
+        lowestPossible()
+        print('the highest possible bet is', lowestPersonsMoney)
     
     if not betThisRound :
         if playerChoice == 'check' :
             done.append(player)
         elif playerChoice == 'bet' :
-            ducatsToBet += int(input('how many ducats do you want to bet'))
+            possibleDucatsToBet = int(input('how many ducats do you want to bet'))
             betThisRound = True
             done = []
             done.append(player)
 
     else:
         if playerChoice == 'raise' :
-            ducatsToBet += int(input('how many ducats do you want to raise by'))
+            possibleDucatsTobet = int(input('how many ducats do you want to raise by, bearing in mind that the current bet is,',ducatsToBet))
             done = []
             done.append(player)
         if playerChoice == 'match' :
             done.append(player)
             
-print(len(done))
-print(len(playersLeft))
-for player in playersLeft :         
-    while len(done) < len(playersLeft):
-        print('player', player.number)
-        playerChoice = input('do you want to match, raise or fold')
-        if playerChoice == 'fold' :
-            playersLeft.remove(player)
-        if playerChoice == 'raise' :
-            ducatsToBet += int(input('how many ducats do you want to bet'))
-            done = []
-            done.append(player)
-        if playerChoice == 'match' :
-            done.append(player)
+while len(done) < len(playersLeft):
+    for i in range(len(playersLeft)-1, -1, -1) :
+        player = playersLeft[i]
+        if not player in done :
+            print('player', player.number)
+            playerChoice = input('do you want to match, raise or fold')
+            if playerChoice == 'fold' :
+                playersLeft.remove(player)
+                lowestPossible()
+            if playerChoice == 'raise' :
+                possibleDucatsToBet= int(input('how many ducats do you want to raise by, bearing in mind that the current bet is,',ducatsToBet))
+                done = []
+                done.append(player)
+            if playerChoice == 'match' :
+                done.append(player)
 
 for player in playersLeft :
     player.money -= ducatsToBet
 
-    
-    
+if 
+
+
         
+        
+            
                     
             
 
