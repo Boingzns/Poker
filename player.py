7e86410c01f4a172b4e19399919f3b2f05556c46
Boingzns <33784478+Boingzns@users.noreply.github.com>
2017-11-18 14:20:50 +0000

    add card and player

---
+++ player.py
@@ -0,0 +1,14 @@
+class Player:
+    population = 0
+
+    def __init__(self):
+        Player.population += 1
+        self.number = Player.population
+        self.money = 5000
+        self.hand = []
+        self.playing = True
+
+    def receivecard(self, newcard):
+        self.hand.append(newcard)
+ 
+
